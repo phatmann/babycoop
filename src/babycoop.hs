488d45d3041d98581d23edeac5f5ff2e8d67e6a4
phatmann <thephatmann@gmail.com>
2013-10-14 10:28:31 -0700

    Cleaner init, persons unchosen

--- src/babycoop.hs
+++ src/babycoop.hs
@@ -3,16 +3,22 @@ module Main where
 import Data.List (sortBy, partition)
 import Data.Function (on)
 
-persons = ["Rebecca", "Jenny", "Kate", "Kasey", "Neha", "Erica"]
+
 
 data Status = Available | Unavailable | Hosting | Requested | Rejected | Chosen | NotChosen deriving (Eq, Show)
 type Date = (Int, Int, Int) -- year, month, date
 type Person = String
-type Availability = (Person, Status)
-type Week = (Date, [Availability])
+type Slot = (Person, Status)
+type Week = (Date, [Slot])
 type Calendar = [Week]
 
-theCalendar = [((2013, 10, 14), [("Rebecca", Available), ("Jenny", Available), ("Kate", Available), ("Kasey", Available), ("Neha", Available), ("Erica", Available)])]
+maxChosen = 3
+maxOuts   = 1
+historyCount = 2
+persons = ["Rebecca", "Jenny", "Kate", "Kasey", "Neha", "Erica"]
+initialSlots = map (\x -> (x, Available)) persons
+
+theCalendar = [((2013, 10, 14), initialSlots), ((2013, 10, 21), initialSlots), ((2013, 10, 28), initialSlots)]
 
 {-
 Rules:
@@ -25,13 +31,11 @@ Rules:
 -}
 
 
-maxChosen = 3
-maxOuts   = 1
-historyCount = 2
+
 
 emptyDate = (0, 0, 0)
-emptyAvail = (emptyDate, [])
-emptyHistory = take historyCount $ repeat emptyAvail
+emptySlot = (emptyDate, [])
+emptyHistory = take historyCount $ repeat emptySlot
 
 updateCalendar :: Calendar -> Calendar
 updateCalendar calendar = updateCalendar' emptyHistory calendar
@@ -43,28 +47,31 @@ updateCalendar' history (week:remainingWeeks) = let updatedWeek = updateWeek his
                                                 in  updatedWeek : updateCalendar' updatedHistory remainingWeeks
 
 updateWeek :: [Week] -> Week -> Week
-updateWeek history (date, avails) = let rankedAvails           = map (pairWithOutCount history) avails
-                                        sortedRankedAvails     = sortBy (compare `on` fst) rankedAvails
-                                        groupedRankedAvails    = partition (isEligible) sortedRankedAvails
-                                        eligibleRankedAvails   = fst groupedRankedAvails
-                                        ineligibleRankedAvails = snd groupedRankedAvails
-                                        (limitedEligibleRankedAvails, restEligibleRankedAvails) = splitAt maxChosen eligibleRankedAvails
-                                        chooseRankedAvailability (rank, (person, status)) = (rank, (person, Chosen))
-                                        chosenRankedAvails   = map chooseRankedAvailability limitedEligibleRankedAvails
-                                        allRankedAvails      = chosenRankedAvails ++ restEligibleRankedAvails ++ ineligibleRankedAvails
-                                    in  (date, map snd allRankedAvails)
+updateWeek history (date, avails) = let rankedSlots           = map (pairWithOutCount history) avails
+                                        sortedRankedSlots     = sortBy (compare `on` fst) rankedSlots
+                                        groupedRankedSlots    = partition (isEligible) sortedRankedSlots
+                                        eligibleRankedSlots   = fst groupedRankedSlots
+                                        ineligibleRankedSlots = snd groupedRankedSlots
+                                        (limitedEligibleRankedSlots, restEligibleRankedSlots) = splitAt maxChosen eligibleRankedSlots
+                                        chooseRankedSlot (rank, (person, status)) = (rank, (person, Chosen))
+                                        unchooseRankedSlot (rank, slot@(person, Available)) = (rank, (person, NotChosen))
+                                        unchooseRankedSlot rankedSlot = rankedSlot
+                                        chosenRankedSlots   = map chooseRankedSlot limitedEligibleRankedSlots
+                                        notChosenRankedSlots = map unchooseRankedSlot $ restEligibleRankedSlots ++ ineligibleRankedSlots
+                                        allRankedSlots      = chosenRankedSlots ++ notChosenRankedSlots
+                                    in  (date, map snd allRankedSlots)
 
-pairWithOutCount :: [Week] -> Availability -> (Int, Availability)
-pairWithOutCount history availability@(person, status) =  let statusHistory = map (lookupStatus person) history
-                                                              wasOut status = status `elem` [Chosen, Requested]
-                                                              outCount = length $ filter wasOut statusHistory
-                                                          in  (outCount, availability)
+pairWithOutCount :: [Week] -> Slot -> (Int, Slot)
+pairWithOutCount history slot@(person, status) =  let statusHistory = map (lookupStatus person) history
+                                                      wasOut status = status `elem` [Chosen, Requested]
+                                                      outCount = length $ filter wasOut statusHistory
+                                                  in  (outCount, slot)
 
-isEligible :: (Int, Availability) -> Bool
-isEligible (outCount, availability@(person, status)) = status == Available && outCount <= maxOuts -- TODO: correct?
+isEligible :: (Int, Slot) -> Bool
+isEligible (outCount, slot@(person, status)) = status == Available && outCount <= maxOuts -- TODO: correct?
 
 lookupStatus :: Person -> Week -> Status
-lookupStatus person week@(_, availabilities) =  let s = lookup person availabilities
+lookupStatus person week@(_, slots) =  let s = lookup person slots
                                                 in case s of
                                                   Nothing     -> Unavailable
                                                   Just status -> status
