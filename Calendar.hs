453968baa9e69b71cae283efac532cb0ffcdbbfb
phatmann <thephatmann@gmail.com>
2013-11-03 14:46:10 -0800

    Using packaged stats

--- Calendar.hs
+++ Calendar.hs
@@ -42,21 +42,21 @@ Rules:
 # Rotate who hosts
 -}
 
-updateCalendar :: StdGen -> [Week] -> [Week]
-updateCalendar randGen calendar = let emptyDate = (0, 0, 0)
-                                      emptyWeek = (emptyDate, [])
-                                      emptyHistory = take personCount $ repeat emptyWeek
-                                   in updateCalendar' emptyHistory randGen calendar
+--updateCalendar :: StdGen -> [Week] -> [Week]
+--updateCalendar randGen calendar = let emptyDate = (0, 0, 0)
+--                                      emptyWeek = (emptyDate, [])
+--                                      emptyHistory = take personCount $ repeat emptyWeek
+--                                   in updateCalendar' emptyHistory randGen calendar
 
-updateCalendar' :: [Week] -> StdGen -> [Week] -> [Week]
-updateCalendar' history randGen [] = []
-updateCalendar' history randGen (week:remainingWeeks) =
-  let (updatedWeek, updatedRandGen) = updateWeek history randGen week
-      updatedHistory                = (drop 1 history) ++ [updatedWeek]
-  in  updatedWeek : updateCalendar' updatedHistory updatedRandGen remainingWeeks
+--updateCalendar' :: [Week] -> StdGen -> [Week] -> [Week]
+--updateCalendar' history randGen [] = []
+--updateCalendar' history randGen (week:remainingWeeks) =
+--  let (updatedWeek, updatedRandGen) = updateWeek history randGen week
+--      updatedHistory                = (drop 1 history) ++ [updatedWeek]
+--  in  updatedWeek : updateCalendar' updatedHistory updatedRandGen remainingWeeks
 
-updateWeek :: [Week] -> StdGen -> Week -> (Week, StdGen)
-updateWeek history randGen (date, slots) =
+updateWeek :: StdGen -> Stats -> Int -> Week -> (Week, StdGen)
+updateWeek randGen stats historyCount  (date, slots) =
   let (present, absent) = partition isPresent slots
                           where isPresent slot = attendance slot /= Absent
 
@@ -65,21 +65,22 @@ updateWeek history randGen (date, slots) =
 
       (shuffledAvailable, updatedRandGen) = shuffle available randGen
 
+      personStat slot = Map.findWithDefault (Stat 0 0 0) (person slot) stats
+
       (hosts, guests) = if needHost then (eligibleHosts, ineligibleHosts) else ([], shuffledAvailable)
                         where needHost                         = not $ any isHost confirmed
                               (eligibleHosts, ineligibleHosts) = partitionEligible 1 favoredHosts unfavoredHosts
                               (favoredHosts, unfavoredHosts)   = partition isFavoredToHost shuffledAvailable
                               isHost slot                      = attendance slot == Host
-                              isFavoredToHost slot             = let (_, inCount, hostCount) = inOutHostCount history (person slot)
-                                                                 in hostCount == 0 && inCount <= (personCount `div` 2)
+                              isFavoredToHost slot             = let stat = personStat slot
+                                                                 in (hostCount stat) == 0 && (inCount stat) <= (personCount `div` 2)
 
       (eligible, notEligible)  = partitionEligible numberEligibleNeeded favored unfavored
                                  where numberEligibleNeeded = max 0 $ (personCount `div` 2) - (length $ filter isIn confirmed)
                                        (favored, unfavored) = partition isFavoredForOut guests
                                        isIn slot            = attendance slot == In || attendance slot == Host
-                                       historyCount         = length history
-                                       isFavoredForOut slot = let (inCount, outCount, _) = inOutHostCount history (person slot)
-                                                              in inCount >= (historyCount `div` 2) || outCount < (historyCount `div` 2)
+                                       isFavoredForOut slot = let stat = personStat slot
+                                                              in (inCount stat) >= (historyCount `div` 2) || (outCount stat) < (historyCount `div` 2)
 
       newlyOut  = map (\slot -> slot {attendance=Out}) eligible
       newlyIn   = map (\slot -> slot {attendance=In}) notEligible
@@ -98,7 +99,7 @@ partitionEligible numberEligibleNeeded favored unfavored =
 
 -- TODO: do one pass through attendance history to gather all stats
 
-gatherStats :: Date -> [Week] -> Stats 
+gatherStats :: Date -> [Week] -> (Stats, Int)
 gatherStats date calendar = 
   let dateIndex = case findIndex (\(d, _) -> d == date) calendar of
         Just n -> n
@@ -120,22 +121,23 @@ gatherStats date calendar =
                       Absent -> oldStat
                       TBD    -> oldStat
                     in Map.alter (\_ -> Just newStat) key stats
-      in foldl gatherWeekStats emptyStats history
+      stats = foldl gatherWeekStats emptyStats history
+      in (stats, length history)
 
-inOutHostCount :: [Week] -> Person -> (Int, Int, Int)
-inOutHostCount history person =
-  let attendanceHistory = map (lookupAttendance person) history
-      inCount           = length $ filter (\attendance -> attendance `elem` [In, Host]) attendanceHistory
-      outCount          = length $ filter (\attendance -> attendance == Out) attendanceHistory
-      hostCount         = length $ filter (\attendance -> attendance == Host) attendanceHistory
-  in  (inCount, outCount, hostCount)
+--inOutHostCount :: [Week] -> Person -> (Int, Int, Int)
+--inOutHostCount history person =
+--  let attendanceHistory = map (lookupAttendance person) history
+--      inCount           = length $ filter (\attendance -> attendance `elem` [In, Host]) attendanceHistory
+--      outCount          = length $ filter (\attendance -> attendance == Out) attendanceHistory
+--      hostCount         = length $ filter (\attendance -> attendance == Host) attendanceHistory
+--  in  (inCount, outCount, hostCount)
 
-lookupAttendance :: Person -> Week -> Attendance
-lookupAttendance targetPerson week@(date, slots) =
-  let result = find (\slot -> (person slot) == targetPerson) slots 
-  in case result of
-    Nothing   -> Absent
-    Just slot -> attendance slot
+--lookupAttendance :: Person -> Week -> Attendance
+--lookupAttendance targetPerson week@(date, slots) =
+--  let result = find (\slot -> (person slot) == targetPerson) slots 
+--  in case result of
+--    Nothing   -> Absent
+--    Just slot -> attendance slot
 
 
 
@@ -151,7 +153,7 @@ theCalendar =
   ,((2013, 10, 14),  [Slot Rebecca Host Confirmed
                      ,Slot Kasey In Confirmed
                      ,Slot Neha In Confirmed
-                     ,Slot Kate In Confirmed
+                     ,Slot Kate Out Confirmed
                      ,Slot Erica Out Confirmed
                      ,Slot Jenny Out Confirmed])
   ,((2013, 10, 21),  [Slot Rebecca Out Confirmed
@@ -176,7 +178,7 @@ theCalendar =
                      ,Slot Kasey TBD Proposed
                      ,Slot Neha TBD Proposed
                      ,Slot Kate TBD Proposed
-                     ,Slot Erica Host Proposed
+                     ,Slot Erica TBD Proposed
                      ,Slot Jenny TBD Proposed])
   ,((2013, 11, 18),  [Slot Rebecca TBD Proposed
                      ,Slot Kasey TBD Proposed
--- console.hs
+++ console.hs
@@ -2,6 +2,7 @@ module Console where
 
 import Calendar
 import System.Random
+import qualified Data.Map as Map
 
 printSlot :: Slot -> IO ()
 printSlot slot = do putStrLn ((show $ person slot) ++ ":" ++ (show $ attendance slot))
@@ -11,8 +12,23 @@ printWeek (date@(year, month, day), slots) = do putStrLn ((show month) ++ "/" ++
                                                 mapM_ printSlot slots
                                                 putStrLn ""
 
+printStat :: (Person, Stat) -> IO ()
+printStat (person, stat) =  do 
+                              let inStr   = show $ inCount stat
+                                  outStr  = show $ outCount stat
+                                  hostStr = show $ hostCount stat
+                              putStrLn $ (show person) ++ ": in=" ++ inStr ++ ", out=" ++ outStr ++ ", host=" ++ hostStr
+
+printStats :: Stats -> IO ()
+printStats stats = do mapM printStat $ Map.toAscList stats
+                      putStrLn ""
+
 main :: IO ()
-main = do -- randGen <- newStdGen
-          let stats = gatherStats (2013, 11, 4) theCalendar
-          print $ show stats
-          --mapM_ printWeek $ updateCalendar randGen theCalendar
+main = do randGen <- newStdGen
+          let date = (2013, 11, 11)
+              (stats, historyCount) = gatherStats date theCalendar
+              Just week = lookup date theCalendar
+              (updatedWeek, _) = updateWeek randGen stats historyCount (date, week)
+          printWeek updatedWeek
+          putStrLn $ "Stats for " ++ (show historyCount) ++ " weeks:"
+          printStats stats
