8c71964099ab88399072397f6f5ef4e0e0210658
phatmann <thephatmann@gmail.com>
2013-11-03 19:42:53 -0800

    Print date range

--- console.hs
+++ console.hs
@@ -14,21 +14,36 @@ printWeek (date@(year, month, day), slots) = do putStrLn ((show month) ++ "/" ++
 
 printStat :: (Person, Stat) -> IO ()
 printStat (person, stat) =  do 
-                              let inStr   = show $ inCount stat
-                                  outStr  = show $ outCount stat
-                                  hostStr = show $ hostCount stat
-                              putStrLn $ (show person) ++ ": in=" ++ inStr ++ ", out=" ++ outStr ++ ", host=" ++ hostStr
+  let inStr   = show $ inCount stat
+      outStr  = show $ outCount stat
+      hostStr = show $ hostCount stat
+  putStrLn $ (show person) ++ ": in=" ++ inStr ++ ", out=" ++ outStr ++ ", host=" ++ hostStr
 
 printStats :: Stats -> IO ()
-printStats stats = do mapM printStat $ Map.toAscList stats
-                      putStrLn ""
+printStats stats = do
+  mapM printStat $ Map.toAscList stats
+  putStrLn ""
+
+printDate :: StdGen -> Date -> IO StdGen
+printDate randGen date = do
+  let (stats, historyCount) = gatherStats date theCalendar
+      Just week = lookup date theCalendar
+      (updatedWeek, updatedRandGen) = updateWeek randGen stats historyCount (date, week)
+  putStrLn $ "Using random seed: " ++ (show randGen)
+  putStrLn $ "Stats for " ++ (show historyCount) ++ " weeks:"
+  printStats stats
+  printWeek updatedWeek
+  return updatedRandGen
+
+printDates :: StdGen -> [Date] -> IO ()
+printDates randGen [] = return ()
+printDates randGen (date:dates) = do
+  randGen' <- printDate randGen date
+  printDates randGen' dates
+  return ()
 
 main :: IO ()
-main = do randGen <- newStdGen
-          let date = (2013, 11, 18)
-              (stats, historyCount) = gatherStats date theCalendar
-              Just week = lookup date theCalendar
-              (updatedWeek, _) = updateWeek randGen stats historyCount (date, week)
-          putStrLn $ "Stats for " ++ (show historyCount) ++ " weeks:"
-          printStats stats
-          printWeek updatedWeek
+main = do
+  randGen <- newStdGen
+  printDates randGen [(2013, 11, 18), (2013, 11, 25), (2013, 12, 2), (2013, 12, 9)]
+  return ()
