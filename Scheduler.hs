8355725597ca262cd747673bc386661c9c598126
phatmann <thephatmann@gmail.com>
2013-11-18 20:32:57 -0800

    Updates working
    Might have exposed algorithm bugs

--- Scheduler.hs
+++ Scheduler.hs
@@ -81,22 +81,23 @@ sameMeeting (Meeting date1 _) (Meeting date2 _) = date1 == date2
 applyAttendanceUpdates :: Calendar -> Date -> [(Person, Attendance)] -> Calendar
 applyAttendanceUpdates calendar date [] = calendar
 applyAttendanceUpdates calendar date attendanceUpdates = 
-  let newStatus (_, attendance) = if attendance == TBD then Proposed else Requested
-      requestSlots = map (\x -> slot (fst x) (snd x) (newStatus x)) attendanceUpdates
+  let requestSlots = map (\x -> slot (fst x) (snd x) Requested) attendanceUpdates
   in mergeRequestCalendar calendar [Meeting date requestSlots]
+
+applyUpdates :: Calendar -> Calendar -> Calendar
+applyUpdates calendar updates = sortBy (compare `on` date) $ unionBy sameMeeting updates calendar
 
 honorRequests :: Meeting -> Meeting -> Meeting
 honorRequests meeting requests =
   let sortByPerson = sortBy (compare `on` person)
-      mergeRequest slot requestSlot = slot {status=Requested, attendance=(attendance requestSlot)}
+      mergeRequest slot requestSlot = slot {status=newStatus, attendance=newAttendance}
+                                        where newAttendance = attendance requestSlot
+                                              newStatus = if newAttendance == TBD then Proposed else Requested
       mergeSlots (slot1, slot2) = if (status slot2 == Requested)
                                   then mergeRequest slot1 slot2
                                   else slot1
       slots' = map mergeSlots $ zip (sortByPerson $ slots meeting) (sortByPerson $ slots requests)
   in meeting {slots = slots'}
-
-applyUpdates :: Calendar -> Calendar -> Calendar
-applyUpdates calendar updates = sortBy (compare `on` date) $ unionBy sameMeeting updates calendar
 
 mergeRequestCalendar :: Calendar -> Calendar -> Calendar
 mergeRequestCalendar calendar requestCalendar = 
